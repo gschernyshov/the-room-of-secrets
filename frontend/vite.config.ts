7cc23b393131bd87fcc853242ee834fc66ee1e32
gschernyshov <grchafv@yandex.ru>
2026-03-05 13:11:54 +0300

    feat(frontend): добавление aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+
+import path from 'path'
 import react from '@vitejs/plugin-react'
+
+const paths = (p: string) => path.resolve(__dirname, p)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': paths('src'),
+      '@/app': paths('src/app'),
+      '@/pages': paths('src/pages'),
+      '@/widgets': paths('src/widgets'),
+      '@/features': paths('src/features'),
+      '@/entities': paths('src/entities'),
+      '@/shared': paths('src/shared'),
+    },
+  },
 })
