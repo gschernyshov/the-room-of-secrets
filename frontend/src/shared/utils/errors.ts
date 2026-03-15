a64a3bd1d86cd9aabb0cf778854faa9d0580760b
gschernyshov <grchafv@yandex.ru>
2026-03-15 13:57:07 +0300

    feat(frontend): добавление safeJsonParse и AppError

---
+++ frontend/src/shared/utils/errors.ts
@@ -0,0 +1,8 @@
+export class AppError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'AppError'
+
+    Object.setPrototypeOf(this, new.target.prototype)
+  }
+}
