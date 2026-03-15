ffda7ef75e2e580ef7f3389f5c3c598014796701
gschernyshov <grchafv@yandex.ru>
2026-03-15 13:34:16 +0300

    feat(backend): добавление cookie-parser middleware для парсинга куки, а также настройка CORS с поддержкой credentials и динамическим origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,20 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
 
 export const app = express()
 
-app.use(cors({ origin: process.env.CORS_ORIGINS || 'http://localhost:3003' }))
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGINS || 'http://localhost:5173',
+    credentials: true,
+  })
+)
+
+app.use(cookieParser())
 app.use(express.json({ limit: '10mb' }))
 
 app.use('/api/auth', authRoutes)
