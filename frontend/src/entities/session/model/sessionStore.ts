4816c27cc05a9d24cc1e456bdee374ffde801f06
gschernyshov <grchafv@yandex.ru>
2026-03-28 18:23:27 +0300

    fix(frontend): обновление sessionStore

---
+++ frontend/src/entities/session/model/sessionStore.ts
@@ -0,0 +1,64 @@
+import { create } from 'zustand'
+import { type User } from '@/entities/user/model/types'
+import { apiFetch } from '@/shared/api/apiFetch'
+import { tokenService } from '@/shared/auth/lib/tokenService'
+import { AppError } from '@/shared/utils/errors'
+
+type SessionStatus = 'authenticated' | 'unauthenticated' | 'loading'
+
+type SessionState = {
+  status: SessionStatus
+  user: User | null
+}
+
+type SessionActions = {
+  login: (token: string, user: User) => void
+  logout: () => void
+  init: () => Promise<void>
+}
+
+export const useSessionStore = create<SessionState & SessionActions>(set => ({
+  status: 'loading',
+  user: null,
+
+  login: (token, user) => {
+    tokenService.set(token)
+    set({ status: 'authenticated', user })
+  },
+
+  logout: () => {
+    tokenService.remove()
+    set({ status: 'unauthenticated', user: null })
+  },
+
+  init: async () => {
+    const token = tokenService.get()
+    if (!token) {
+      set({ status: 'unauthenticated', user: null })
+      return
+    }
+
+    try {
+      const response = await apiFetch('/user/me')
+      const result = await response.json()
+
+      if (result.success) {
+        set({ status: 'authenticated', user: result.data })
+      } else {
+        throw new AppError(
+          'При аутентификации пользователя возникала непредвиденная ошибка',
+          result.error.message
+        )
+      }
+    } catch (error) {
+      tokenService.remove()
+      set({ status: 'unauthenticated', user: null })
+
+      if (error instanceof AppError) throw error
+
+      throw new AppError(
+        'При аутентификации пользователя возникала непредвиденная ошибка'
+      )
+    }
+  },
+}))
