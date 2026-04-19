fb97efde7f078ba0514b07e522d1e0a2acf35e79
gschernyshov <grchafv@yandex.ru>
2026-04-19 15:40:15 +0300

    fix(frontend): форматирование событий сущности Room

--- frontend/src/entities/room/model/roomEvents.ts
+++ frontend/src/entities/room/model/roomEvents.ts
@@ -1,10 +1,6 @@
 export const ROOM_EVENTS = {
-  CREATE_ROOM: 'create_room',
   JOIN_ROOM: 'join_room',
   LEAVE_ROOM: 'leave_room',
   USER_JOINED: 'user_joined',
   USER_LEFT: 'user_left',
-
-  SEND_MESSAGE: 'send_message',
-  NEW_MESSAGE: 'new_message',
 } as const
