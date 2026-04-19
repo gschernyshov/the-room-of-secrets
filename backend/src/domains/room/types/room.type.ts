f53032a2f6e0898558425e3adba4f5ac7045e656
gschernyshov <grchafv@yandex.ru>
2026-04-19 14:17:33 +0300

    fix(backend): обновление типа Room (расширение свойства participants)

--- backend/src/domains/room/types/room.type.ts
+++ backend/src/domains/room/types/room.type.ts
@@ -1,9 +1,16 @@
 import { UUID } from 'node:crypto'
 import { User } from '../../user/types/user.type.js'
 
+export type ParticipantStatus = 'active' | 'left' | 'kicked' | 'banned'
+
+export type RoomParticipant = {
+  userId: User['id']
+  status: ParticipantStatus
+}
+
 export type Room = {
   id: UUID
   name: string
-  participants: User['id'][]
+  participants: RoomParticipant[]
   createdAt: Date
 }
