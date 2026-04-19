f666753ee2c56ce992e77fde16d60dffaa7c0711
gschernyshov <grchafv@yandex.ru>
2026-04-19 14:16:37 +0300

    fix(backend): обновление типа User (добавление ключей role и status)

--- backend/src/domains/user/types/user.type.ts
+++ backend/src/domains/user/types/user.type.ts
@@ -1,7 +1,16 @@
+import { type Room } from '../../room/types/room.type.js'
+
+export type Role = 'user' | 'admin'
+
+export type Status = 'active' | 'blocked' | 'deleted'
+
 export type User = {
   id: number
   username: string
   email: string
   password: string
+  role: Role
+  status: Status
+  rooms: Room['id'][]
   createdAt: Date
 }
