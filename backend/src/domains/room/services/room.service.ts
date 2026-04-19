f1440902d505382b87da074d4214ff1145f565a9
gschernyshov <grchafv@yandex.ru>
2026-04-19 14:34:08 +0300

    feat(backend): реализованы методы leave и getRoomsByUser в roomService

--- backend/src/domains/room/services/room.service.ts
+++ backend/src/domains/room/services/room.service.ts
@@ -2,8 +2,6 @@ import { ROOM_CREATED, ROOM_JOINED } from '../events/index.js'
 import { roomRepository } from '../repositories/room.repository.js'
 import { type Room } from '../types/room.type.js'
 import { type User } from '../../user/types/user.type.js'
-import { messageRepository } from '../../message/repositories/message.repository.js'
-import { type Message } from '../../message/types/message.type.js'
 import { eventBus } from '../../../infrastructure/events/eventBus.js'
 import { logger } from '../../../shared/utils/logger.js'
 import { AppError } from '../../../shared/utils/errors.js'
@@ -15,7 +13,7 @@ export const roomService = {
     try {
       const room = await roomRepository.create(name, creatorId)
       if (!room) {
-        throw new AppError('Не удалось создать команту', 500)
+        throw new AppError('Не удалось создать комнату', 500)
       }
 
       eventBus.emit(ROOM_CREATED, room)
@@ -23,7 +21,7 @@ export const roomService = {
       return room
     } catch (error) {
       logger.error(
-        `При создании комнаты "${name}" пользователем id: ${creatorId} возникла ошибка: ${error.message.toLowerCase()}`
+        `При создании комнаты "${name}" пользователем id: ${creatorId} возникла ошибка${error instanceof Error ? `: ${error.message}` : ``}}`
       )
 
       if (error instanceof AppError) {
@@ -37,10 +35,7 @@ export const roomService = {
     }
   },
 
-  join: async (
-    roomId: Room['id'],
-    userId: User['id']
-  ): Promise<{ room: Room; messages: Message[] }> => {
+  join: async (roomId: Room['id'], userId: User['id']): Promise<Room> => {
     logger.info(
       `Пользователь id: ${userId} присоединяется к комнате id: ${roomId}`
     )
@@ -51,18 +46,14 @@ export const roomService = {
         throw new AppError('Комната не найдена', 401)
       }
 
-      if (!room.participants.includes(userId)) {
-        await roomRepository.join(roomId, [...room.participants, userId])
-      }
-
-      const messages = await messageRepository.get(roomId)
+      await roomRepository.join(roomId, userId)
 
       eventBus.emit(ROOM_JOINED, { roomId, name: room.name, userId })
 
-      return { room, messages }
+      return room
     } catch (error) {
       logger.error(
-        `При присоединении пользователя id: ${userId} к комнате id: ${roomId} возникла ошибка: ${error.message.toLowerCase()}`
+        `При присоединении пользователя id: ${userId} к комнате id: ${roomId} возникла ошибка${error instanceof Error ? `: ${error.message}` : ``}}`
       )
 
       if (error instanceof AppError) {
@@ -70,7 +61,66 @@ export const roomService = {
       }
 
       throw new AppError(
-        'При попытке присоединения к комнате возникла непредвиденная ошибка'
+        'При присоединении к комнате возникла непредвиденная ошибка',
+        500
+      )
+    }
+  },
+
+  leave: async (roomId: Room['id'], userId: User['id']): Promise<void> => {
+    logger.info(`Пользователь id: ${userId} выходит из комнаты id: ${roomId}`)
+
+    try {
+      const rooms = await roomRepository.leave(roomId, userId)
+      if (!rooms) {
+        throw new AppError('Не удалось выйти из команты', 500)
+      }
+
+      // Проверяем, ушли ли ВСЕ участники из комнаты:
+      // Если у каждого участника статус 'left', значит, комната пуста и ее можно удалить
+      const deleted = rooms.participants.every(
+        participant => participant.status === 'left'
+      )
+
+      if (deleted) await roomRepository.delete(roomId)
+    } catch (error) {
+      logger.error(
+        `При выходе из комнаты id: ${roomId} пользователя id: ${userId} возникла ошибка${error instanceof Error ? `: ${error.message}` : ``}}`
+      )
+
+      if (error instanceof AppError) {
+        throw error
+      }
+
+      throw new AppError(
+        'При выходе из комнаты возникла непредвиденная ошибка',
+        500
+      )
+    }
+  },
+
+  getRoomsByUser: async (userId: User['id']): Promise<Room[]> => {
+    logger.info(`Получение комнат пользователя id: ${userId}`)
+
+    try {
+      const rooms = await roomRepository.getRoomsByUser(userId)
+      if (!rooms) {
+        throw new AppError('Не удалось найти комнаты', 404)
+      }
+
+      return rooms
+    } catch (error) {
+      logger.error(
+        `При получении комнат пользователя id: ${userId} возникла ошибка${error instanceof Error ? `: ${error.message}` : ``}}`
+      )
+
+      if (error instanceof AppError) {
+        throw error
+      }
+
+      throw new AppError(
+        'При получении комнат возникла непредвиденная ошибка',
+        500
       )
     }
   },
