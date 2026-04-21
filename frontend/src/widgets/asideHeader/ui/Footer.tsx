f3536a2367be4baca53334bec03ff874a686ad6b
gschernyshov <grchafv@yandex.ru>
2026-04-21 10:01:51 +0300

    feat(frontend): добавление UI-состояния socket-подключения

--- frontend/src/widgets/asideHeader/ui/Footer.tsx
+++ frontend/src/widgets/asideHeader/ui/Footer.tsx
@@ -1,6 +1,7 @@
 import { Avatar } from '@gravity-ui/uikit'
 import { useSessionStore } from '@/entities/session/model/sessionStore'
 import { LogoutButton } from '@/features/auth/logout/ui/LogoutButton'
+import { useSocketStore } from '@/shared/store/socketStore'
 import styles from './Footer.module.scss'
 
 type Props = {
@@ -10,13 +11,24 @@ type Props = {
 
 export const Footer = ({ compact, handleProfile }: Props) => {
   const { status, user } = useSessionStore()
+  const { isConnecting, isConnected } = useSocketStore()
 
   if (status !== 'authenticated') return null
 
   return (
     <div className={styles['footer']}>
       <div className={styles['footer__profile']} onClick={handleProfile}>
-        <Avatar text={user?.username ?? 'Аватар'} size={compact ? 'm' : 'xl'} />
+        <Avatar
+          text={user?.username ?? 'Аватар'}
+          size={compact ? 'm' : 'xl'}
+          className={
+            isConnecting
+              ? styles['footer__avatar--loading']
+              : isConnected
+                ? styles['footer__avatar--active']
+                : styles['footer__avatar--passive']
+          }
+        />
         {!compact && (
           <div className={styles['footer__info']}>
             <span className={styles['footer__username']}>
