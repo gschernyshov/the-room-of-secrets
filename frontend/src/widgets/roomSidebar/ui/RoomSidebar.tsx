f3b3f2c614a6dcb8e753ec53ebea7941f813df0a
gschernyshov <grchafv@yandex.ru>
2026-05-20 09:24:34 +0300

    fix(frontend): улучшение UI виджета RoomSidebar

--- frontend/src/widgets/roomSidebar/ui/RoomSidebar.tsx
+++ frontend/src/widgets/roomSidebar/ui/RoomSidebar.tsx
@@ -1,3 +1,6 @@
+import clsx from 'clsx'
+import { ExitRoom } from '@/features/room/exitRoom'
+import { LeaveRoom } from '@/features/room/leaveRoom'
 import { useSessionStore } from '@/entities/session/model/sessionStore'
 import { useRoomStore } from '@/entities/room/model/roomStore'
 import { usePresenceStore } from '@/entities/room/model/presenceStore'
@@ -8,26 +11,56 @@ export const RoomSidebar = () => {
   const currentRoom = useRoomStore(state => state.currentRoom)
   const onlineUserIds = usePresenceStore(state => state.onlineUserIds)
 
+  if (!currentRoom) return null
+
   return (
     <div className={styles['room-sidebar']}>
       <h3 className={styles['room-sidebar__title']}>
-        Участники ({currentRoom?.participants.length})
+        Участники ({currentRoom.participants.length})
       </h3>
 
-      <ul>
-        {currentRoom?.participants.map(participant => (
-          <li key={participant.userId}>
-            Пользователь {participant.userId}
-            {participant.userId === user?.id && ' (Вы)'}
-            {participant.userId !== user?.id && (
-              <span>
-                {' '}
-                — {onlineUserIds.has(participant.userId) ? '🟢' : '⚪'}
-              </span>
+      <ul className={styles['room-sidebar__list']}>
+        {user && currentRoom.participants.some(p => p.userId === user.id) && (
+          <li
+            className={clsx(
+              styles['room-sidebar__item'],
+              styles['room-sidebar__item--current']
             )}
+          >
+            Пользователь {user.id} <span>(Вы)</span>
           </li>
-        ))}
+        )}
+        {currentRoom.participants.map(
+          participant =>
+            participant.userId !== user?.id && (
+              <li
+                key={participant.userId}
+                className={clsx(styles['room-sidebar__item'])}
+              >
+                Пользователь {participant.userId}
+                {participant.userId === user?.id ? (
+                  ' (Вы)'
+                ) : (
+                  <span
+                    className={clsx(
+                      styles['room-sidebar__item-status'],
+                      onlineUserIds.has(participant.userId) &&
+                        styles['room-sidebar__item-status--active']
+                    )}
+                  >
+                    {onlineUserIds.has(participant.userId)
+                      ? ' (в сети)'
+                      : ' (не в сети)'}
+                  </span>
+                )}
+              </li>
+            )
+        )}
       </ul>
+      <div className={styles['room-sidebar__actions']}>
+        <ExitRoom roomId={currentRoom.id} />
+        <LeaveRoom roomId={currentRoom.id} />
+      </div>
     </div>
   )
 }
