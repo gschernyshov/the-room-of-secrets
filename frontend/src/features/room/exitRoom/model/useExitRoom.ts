91b4a121d4c53a6464aa3746b69438699779e4cf
gschernyshov <grchafv@yandex.ru>
2026-05-20 09:20:53 +0300

    feat(frontend): добавление состояния загрузки в хуке useExitRoom фичи ExitRooms

--- frontend/src/features/room/exitRoom/model/useExitRoom.ts
+++ frontend/src/features/room/exitRoom/model/useExitRoom.ts
@@ -1,16 +1,28 @@
+import { useState } from 'react'
 import { exitRoom } from './exitRoom'
 import { type Room } from '@/entities/room/model/types'
+import { AppError } from '@/shared/utils/errors'
 
 export const useExitRoom = () => {
+  const [isLoading, setIsLoading] = useState(false)
+
   const exit = async (id: Room['id']) => {
+    if (isLoading) return
+
+    setIsLoading(true)
     try {
       await exitRoom(id)
-    } catch {
-      /* empty */
+    } catch (error) {
+      if (error instanceof AppError) throw error
+
+      throw new AppError('При выходе из комнаты возникла непредвиденная ошибка')
+    } finally {
+      setIsLoading(false)
     }
   }
 
   return {
+    isLoading,
     exit,
   }
 }
